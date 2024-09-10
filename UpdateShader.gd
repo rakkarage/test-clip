e9f6ade325220616b0bcfb67ef995d99c778acb3
Rakka Rage <rakkarage@gmail.com>
2024-09-10 16:07:14 -0400

    transform

--- UpdateShader.gd
+++ UpdateShader.gd
@@ -1,14 +1,14 @@
 extends Node3D
 
-@onready var _parent = %Mask
-@onready var _child = %Masked
+@onready var _mask = %Mask
+@onready var _masked = %Masked
 
 func _process(_delta: float) -> void:
-	var mat = _child.get_surface_override_material(0)
+	var mat = _masked.get_surface_override_material(0)
 	if mat:
-		var pos: Vector3 = _parent.global_transform.origin
-		var size: Vector2 = _parent.mesh.size
-		print("pos: ", pos)
-		print("size: ", size)
-		mat.set_shader_parameter("mask_position", pos)
-		mat.set_shader_parameter("mask_size", size)
+		var t: Transform3D = _mask.global_transform
+		var s: Vector2 = _mask.mesh.size
+		print("pos: ", t)
+		print("size: ", s)
+		mat.set_shader_parameter("mask_transform", t)
+		mat.set_shader_parameter("mask_size", s)
